165850c5fd7a6b2a81e078fde754f4f957e5a1e4
dmachard <5562930+dmachard@users.noreply.github.com>
2026-06-20 19:12:09 +0200

    feat: add CAM and TS quality detection support to parser and update test cases

--- releascenify/parser.py
+++ releascenify/parser.py
@@ -139,7 +139,7 @@ class ReleaseParser:
             # Languages
             'FRENCH', 'TRUEFRENCH', 'MULTI', 'VOSTFR', 'VOST', 'VFF', 'VFI', 'VFQ', 'VF2', 'VFR', 'SUBFRFORCED', 'FORCED', 'SUBFR', 'SUBFORCED',
             # Source/Quality
-            'BLURAY', 'BDRIP', 'BRRIP', 'WEBDL', 'WEB-DL', 'WEBRIP', 'DVDRIP', 'HDRIP', 'HDTV',
+            'BLURAY', 'BDRIP', 'BRRIP', 'WEBDL', 'WEB-DL', 'WEBRIP', 'DVDRIP', 'HDRIP', 'HDTV', 'CAM', 'HDCAM', 'CAMRIP', 'TS', 'TELESYNC',
             # Audio
             'AC3', 'EAC3', 'DTS', 'AAC', 'MP3', 'FLAC', 'ATMOS', 'TRUEHD', 'DDP', 'HDMA', 'DTSHD', 'DTSHDMA',
             # Other common properties
@@ -325,7 +325,7 @@ class ReleaseParser:
                     # Resolutions
                     '1080P', '720P', '2160P', '4K', 'UHD', '4KLIGHT', '576P', '480P',
                     # Source/Quality
-                    'REMUX', 'BLURAY', 'BDRIP', 'BRRIP', 'WEBDL', 'WEB-DL', 'WEBRIP', 'WEBLIGHT', 'WEB', 'DVDRIP', 'HDRIP', 'HDTV', 'HD', 'SDR',
+                    'REMUX', 'BLURAY', 'BDRIP', 'BRRIP', 'WEBDL', 'WEB-DL', 'WEBRIP', 'WEBLIGHT', 'WEB', 'DVDRIP', 'HDRIP', 'HDTV', 'HD', 'SDR', 'CAM', 'HDCAM', 'CAMRIP', 'TS', 'TELESYNC',
                     # Audio
                     'AC3', 'EAC3', 'DTS', 'AAC', 'MP3', 'FLAC', 'ATMOS', 'TRUEHD', 'DDP', 'HDMA', 'DTSHD', 'DTSHDMA',
                     # Languages
@@ -494,12 +494,25 @@ class ReleaseParser:
 
     def _extract_quality(self, filename: str, result: Dict[str, Any]):
         """Extracts source quality with priority matching."""
-        for pat in [r'(?i)(REMUX)', r'(?i)(BLU[\.\-]?RAY|BDRIP|BRRIP)', r'(?i)(WEB-DL|WEBDL|WEBRIP|WEBLIGHT|WEB)', r'(?i)(DVDRIP)', r'(?i)(HDRIP)', r'(?i)(HDTV)']:
+        for pat in [
+            r'(?i)(REMUX)',
+            r'(?i)(BLU[\.\-]?RAY|BDRIP|BRRIP)',
+            r'(?i)(WEB-DL|WEBDL|WEBRIP|WEBLIGHT|WEB)',
+            r'(?i)(DVDRIP)',
+            r'(?i)(HDRIP)',
+            r'(?i)(HDTV)',
+            r'(?i)\b(HDCAM|CAMRIP|CAM)\b',
+            r'(?i)\b(TELESYNC|TS)\b'
+        ]:
             match = re.search(pat, filename)
             if match:
                 qual_raw = match.group(1).upper().replace('-', '').replace('.', '')
                 if qual_raw in ('WEBDL', 'WEB'):
                     result['quality'] = 'WEB-DL'
+                elif qual_raw in ('HDCAM', 'CAMRIP', 'CAM'):
+                    result['quality'] = 'CAM'
+                elif qual_raw in ('TELESYNC', 'TS'):
+                    result['quality'] = 'TS'
                 else:
                     result['quality'] = qual_raw
                 break
@@ -511,7 +524,7 @@ class ReleaseParser:
 
         tags_to_split = [
             r'S\d+', r'E\d+', r'S\d+(?:E\d+)+', r'\d{1,2}x\d{1,3}', r'SAISON[\.\-\s]?\d+', r'EPISODE[\.\-\s]?\d+', 'MULTI', 'FRENCH', 'TRUEFRENCH', 'VOSTFR', 'SUBFRENCH', 'SUBBED', 'SUBS', 'MSUB', 'VFF', 'VFI', 'VFQ', 'VF2', 'VFR', 'VOST', 'STFI', 'FASTSUB', 'SUBFRFORCED', 'FORCED', 'SUBFR', 'SUBFORCED',
-            '1080P', '720P', '2160P', '4K', '4KLIGHT', 'UHD', 'M4K', 'M1080P', 'M720P', 'MHD', 'BLURAY', 'BDRIP', 'DVDRIP', 'HDRIP', 'WEBRIP', 'WEB-DL', 'WEBDL', 'WEBLIGHT', 'WEB',
+            '1080P', '720P', '2160P', '4K', '4KLIGHT', 'UHD', 'M4K', 'M1080P', 'M720P', 'MHD', 'BLURAY', 'BDRIP', 'DVDRIP', 'HDRIP', 'WEBRIP', 'WEB-DL', 'WEBDL', 'WEBLIGHT', 'WEB', 'CAM', 'HDCAM', 'CAMRIP', 'TS', 'TELESYNC',
             'HDR', 'HDR10', 'HDR10+', '10BIT', '10BITS', '12BIT', '12BITS', 'SDR', 'DV', 'HEVC', 'X264', 'X265', 'H264', 'H265', 'VC1', 'VC-1', 'REPACK', 'PROPER', 'FINAL', 'INTERNAL', 'CUSTOM', 'AC3', 'DDP', 'DTSHDMA', 'DTS', 'ATMOS', 'FLAC', 'MP3', 'HDMA',
             'NF', 'AMZN', 'DSNP', 'ATV', 'DSNY', 'HMAX', 'HBO', 'HULU', 'REMUX',
             'FILM', 'SILENT', 'FANEDIT',
@@ -558,7 +571,7 @@ class ReleaseParser:
         post_se = filename[se_match.end():]
         tags_to_split = [
             r'\d{1,2}x\d{1,3}', 'MULTI', 'FRENCH', 'TRUEFRENCH', 'VOSTFR', 'SUBFRENCH', 'SUBBED', 'SUBS', 'MSUB', 'VFF', 'VFI', 'VFQ', 'VF2', 'VFR', 'VOST', 'STFI', 'FASTSUB', 'SUBFRFORCED', 'FORCED', 'SUBFR', 'SUBFORCED',
-            '1080P', '720P', '2160P', '4K', '4KLIGHT', 'UHD', 'M4K', 'M1080P', 'M720P', 'MHD', 'BLURAY', 'BDRIP', 'DVDRIP', 'HDRIP', 'WEBRIP', 'WEB-DL', 'WEBDL', 'WEBLIGHT', 'WEB',
+            '1080P', '720P', '2160P', '4K', '4KLIGHT', 'UHD', 'M4K', 'M1080P', 'M720P', 'MHD', 'BLURAY', 'BDRIP', 'DVDRIP', 'HDRIP', 'WEBRIP', 'WEB-DL', 'WEBDL', 'WEBLIGHT', 'WEB', 'CAM', 'HDCAM', 'CAMRIP', 'TS', 'TELESYNC',
             'HDR', 'HDR10', 'HDR10+', '10BIT', '10BITS', '12BIT', '12BITS', 'SDR', 'DV', 'HEVC', 'X264', 'X265', 'H264', 'H265', 'VC1', 'VC-1', 'REPACK', 'PROPER', 'FINAL', 'INTERNAL', 'CUSTOM', 'AC3', 'DDP', 'DTSHDMA', 'DTS', 'ATMOS', 'FLAC', 'MP3', 'HDMA',
             'NF', 'AMZN', 'DSNP', 'ATV', 'DSNY', 'HMAX', 'HBO', 'HULU', 'REMUX',
             'FILM', 'SILENT', 'FANEDIT',
